41fa1ce0dd5316ddfaae578ff18fc01a459fb82d
Akshay Pandit <panditakshay402@gmail.com>
2024-08-09 04:34:35 +0530

    just added login and register page

---
+++ screens/auth/Login.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Login = () => {
+  return (
+    <View>
+      <Text>Login to your account.</Text>
+      <Text>Please sign in to your account </Text>
+      <Text>Email </Text>
+    </View>
+  )
+}
+
+export default Login
---
+++ screens/auth/Register.js
@@ -0,0 +1,14 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const Register = () => {
+  return (
+    <View>
+      <Text>Create your new account.</Text>
+      <Text>Create an account to start looking for the food you like </Text>
+      
+    </View>
+  )
+}
+
+export default Register
